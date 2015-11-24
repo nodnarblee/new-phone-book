787e300ab0a83909d80c16fb6a69b935f37d9a0f
Brandon <blee298@gmail.com>
2015-11-23 22:47:45 -0800

    adding previously untracked files

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-var connect = require('connect');
+var express = require('express');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/public')).listen(8080);
+var app = express();
+
+app.use(serveStatic(__dirname + '/node_modules/angular'));
+app.use(serveStatic(__dirname + '/node_modules/angular-route'));
+app.use(serveStatic(__dirname + '/node_modules/angular-resource'));
+app.use(serveStatic(__dirname + '/public'));
+app.listen(8080);
